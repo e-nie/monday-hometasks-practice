f0b76798a1125a9adb9e2dbef2db80f064a0ce3d
Eva Niehaus <eniehaus111@gmail.com>
2023-06-21 02:00:22 +0200

    add use State

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
+import {useState} from "react";
 
 
 function App() {
 
+    // let a = 1
+    let [a, setA] = useState(1)
+    const onClickHandler = () => {
+       setA(++a)
+
+    }
+    const resetOnClickHandler = () => {
+        setA(0)
+    }
     return (
         <div className = "App">
-
-
+            <h1>{a}</h1>
+            <button onClick = {onClickHandler}>number</button>
+            <button onClick = {resetOnClickHandler}>0</button>
         </div>
     );
 }
